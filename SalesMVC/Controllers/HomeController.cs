d00631290751e9ca2b317c95001bb099b9b6d9f7
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-07-06 12:12:24 -0300

    Model-View-Controller: Department

--- SalesMVC/Controllers/HomeController.cs
+++ SalesMVC/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesMVC.Models;
+using SalesMVC.Models.ViewModels;
 
 namespace SalesMVC.Controllers
 {
---
+++ SalesMVC/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SalesMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
