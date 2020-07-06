ad55fce4ee4cd4bbc82fe94b7be99eb7d602f45b
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-07-06 13:41:27 -0300

    Add other entities e second migration

--- SalesMVC/Data/SalesMVCContext.cs
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SalesMVC.Models
 {
     public class SalesMVCContext : DbContext
     {
-        public SalesMVCContext (DbContextOptions<SalesMVCContext> options)
+        public SalesMVCContext(DbContextOptions<SalesMVCContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
