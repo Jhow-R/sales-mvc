3c76c4e54f86326a006dd4735a486121c88084be
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-07-06 17:37:41 -0300

    Seller details with eager loading

--- SalesMVC/Services/SellerService.cs
+++ SalesMVC/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using SalesMVC.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesMVC.Services
 {
@@ -18,9 +19,21 @@ namespace SalesMVC.Services
             return _context.Seller.ToList();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(seller => seller.Department).FirstOrDefault(seller => seller.Id == id);
+        }
+
         public void Insert(Seller seller)
         {
             _context.Add(seller);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
             _context.SaveChanges();
         }
     }
